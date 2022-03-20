26fc73ff47101f01bbd712c92a0cb2d16f0e6ae9
Дмитро Белявський <troyqa@MacBook-Air-Maria.local>
2022-03-20 13:32:30 +0200

    filters shop-page

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,149 @@
 $(function () {
+  $('.hero-slider__list').slick({
+    arrows: true,
+    prevArrow: '<button class="hero-slider__arrow hero-slider__arrow--prev" type="button"><svg><use xlink: href = "../images/sprite.svg#icon-slick-prev"></use></svg></button > ',
+    nextArrow: '<button class="hero-slider__arrow hero-slider__arrow--next" type="button"><svg><use xlink: href = "../images/sprite.svg#icon-slick-next"></use></svg></button>',
+    infinite: false,
+    responsive: [{
+      breakpoint: 992,
+      settings: {
+        arrows: false,
+        dots: true,
+      }
+    }, ]
+  });
 
+  $('.catalog__btn').on('click', function () {
+    $('.catalog__list').toggleClass('catalog__list--active'),
+      $('.catalog__btn').toggleClass('catalog__btn--active')
+  });
+
+  $('.burger').on('click', function () {
+    $('.popup-menu').toggleClass('popup-menu--active'),
+      $('.burger').toggleClass('burger--active'),
+      $('.catalog__list').removeClass('catalog__list--active'),
+      $('body').toggleClass('lock')
+  });
+
+  $('.basket--activator').on('click', function () {
+    $('.basket').toggleClass('basket--active'),
+      $('body').toggleClass('lock')
+  });
+
+  $('.basket__close').on('click', function () {
+    $('.basket').toggleClass('basket--active')
+  });
+
+  $('.user-nav__btn--search').on('click', function () {
+    $('.header__form-search').toggleClass('header__form-search--active')
+  });
+
+  $('.filter__btn').on('click', function () {
+    $('.filter__wrapper').toggleClass('filter__wrapper--active')
+  });
+
+  $('.shop-content__sort').styler();
+
+
+  $(function () {
+    var $overlay = $('.overlay'),
+      $overlayTrigger = $('.burger'),
+      $overlayClose = $('.burger-close'),
+      openClass = 'is-open';
+
+    $overlayTrigger.on('click', function () {
+      var num = ('0' + ($(this).index() + 1)).slice(-2);
+      $('.overlay').addClass(openClass);
+      $overlayClose.addClass(openClass);
+    });
+
+    $overlayClose.on('click', function () {
+      $overlayClose.removeClass(openClass);
+      $overlay.removeClass(openClass);
+    });
+  });
+
+  var $range = $(".filter-price__range"),
+    $inputFrom = $(".filter-price__input--from"),
+    $inputTo = $(".filter-price__input--to"),
+    instance,
+    min = 0,
+    max = 1000,
+    from = 0,
+    to = 0;
+
+  $range.ionRangeSlider({
+    type: "double",
+    onStart: updateInputs,
+    onChange: updateInputs
+  });
+  instance = $range.data("ionRangeSlider");
+
+  function updateInputs(data) {
+    from = data.from;
+    to = data.to;
+
+    $inputFrom.prop("value", from);
+    $inputTo.prop("value", to);
+  }
+
+  $inputFrom.on("input", function () {
+    var val = $(this).prop("value");
+
+    if (val < min) {
+      val = min;
+    } else if (val > to) {
+      val = to;
+    }
+
+    instance.update({
+      from: val
+    });
+  });
+
+  $inputTo.on("input", function () {
+    var val = $(this).prop("value");
+
+    if (val < from) {
+      val = from;
+    } else if (val > max) {
+      val = max;
+    }
+
+    instance.update({
+      to: val
+    });
+  });
+
+  var containerEl1 = document.querySelector('[data-ref="container-1"]');
+  var containerEl2 = document.querySelector('[data-ref="container-2"]');
+
+  var config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+
+  var mixer1 = mixitup(containerEl1, config);
+  var mixer2 = mixitup(containerEl2, config);
+
+  const swiper = new Swiper(".brands", {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    breakpoints: {
+      560: {
+        slidesPerView: 3,
+      },
+      776: {
+        slidesPerView: 4,
+      },
+      992: {
+        centeredSlides: true,
+        slidesPerView: 6,
+        spaceBetween: 30,
+        freeMode: true,
+        loop: true,
+      },
+    }
+  });
 })
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,19 @@
-const { src, dest, watch, parallel, series } = require('gulp');
+const {
+  src,
+  dest,
+  watch,
+  parallel,
+  series
+} = require('gulp');
+const fileInclude = require('gulp-file-include');
 const scss = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer');
 const uglify = require('gulp-uglify');
 const imagemin = require('gulp-imagemin');
+const svgSprite = require('gulp-svg-sprite');
+const cheerio = require('gulp-cheerio');
+const replace = require('gulp-replace');
 const del = require('del');
 const browserSync = require('browser-sync').create();
 
@@ -13,74 +23,133 @@ function browsersync() {
       baseDir: 'app/'
     },
     notify: false
-  })
+  });
+}
+
+const htmlInclude = () => {
+  return src(['app/html/*.html'])
+    .pipe(fileInclude({
+      prefix: '@',
+      basepath: '@file',
+    }))
+    .pipe(dest('app/'))
+    .pipe(browserSync.stream());
 }
 
 function styles() {
   return src('app/scss/style.scss')
-    .pipe(scss({ outputStyle: 'compressed' }))
+    .pipe(scss({
+      outputStyle: 'compressed'
+    }))
     .pipe(concat('style.min.css'))
     .pipe(autoprefixer({
       overrideBrowserslist: ['last 10 versions'],
       grid: true
     }))
     .pipe(dest('app/css'))
-    .pipe(browserSync.stream())
+    .pipe(browserSync.stream());
 }
 
 function scripts() {
   return src([
-    'node_modules/jquery/dist/jquery.js',
-    'app/js/main.js'
-  ])
+      'node_modules/jquery/dist/jquery.js',
+      'node_modules/slick-carousel/slick/slick.min.js',
+      'node_modules/mixitup/dist/mixitup.min.js',
+      'node_modules/swiper/swiper-bundle.min.js',
+      'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+      'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
+      'app/js/main.js'
+    ])
     .pipe(concat('main.min.js'))
     .pipe(uglify())
     .pipe(dest('app/js'))
-    .pipe(browserSync.stream())
+    .pipe(browserSync.stream());
 }
 
 function images() {
   return src('app/images/**/*.*')
     .pipe(imagemin([
-      imagemin.gifsicle({ interlaced: true }),
-      imagemin.mozjpeg({ quality: 75, progressive: true }),
-      imagemin.optipng({ optimizationLevel: 5 }),
+      imagemin.gifsicle({
+        interlaced: true
+      }),
+      imagemin.mozjpeg({
+        quality: 75,
+        progressive: true
+      }),
+      imagemin.optipng({
+        optimizationLevel: 5
+      }),
       imagemin.svgo({
-        plugins: [
-          { removeViewBox: true },
-          { cleanupIDs: false }
+        plugins: [{
+            removeViewBox: true
+          },
+          {
+            cleanupIDs: false
+          }
         ]
       })
     ]))
-    .pipe(dest('dist/images'))
+    .pipe(dest('dist/images'));
+}
+
+function svgSprites() {
+  return src('app/images/icons/*.svg')
+    .pipe(cheerio({
+      run: ($) => {
+        $("[fill]").removeAttr("fill");
+        $("[stroke]").removeAttr("stroke");
+        $("[style]").removeAttr("style");
+      },
+      parserOptions: {
+        xmlMode: true
+      },
+    }))
+    .pipe(replace('&gt;', '>'))
+    .pipe(
+      svgSprite({
+        mode: {
+          stack: {
+            sprite: '../sprite.svg',
+          },
+        },
+      })
+    )
+    .pipe(dest('app/images'));
 }
 
 function build() {
   return src([
-    'app/**/*.html',
-    'app/css/style.min.css',
-    'app/js/main.min.js'
-  ], { base: 'app' })
-    .pipe(dest('dist'))
+      'app/html/**/*.html',
+      'app/css/style.min.css',
+      'app/js/main.min.js'
+    ], {
+      base: 'app'
+    })
+    .pipe(dest('dist'));
 }
 
 function cleanDist() {
-  return del('dist')
+  return del('dist');
 }
 
 function watching() {
   watch(['app/scss/**/*.scss'], styles);
   watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
-  watch(['app/**/*.html']).on('change', browserSync.reload)
+  watch(['app/images/icons/*.svg'], svgSprites);
+  watch(['app/html/**/*.html'], htmlInclude);
+  watch(['app/html/**/*.html']).on('change', browserSync.reload);
+  watch(['app/scss/**/*.scss']).on('change', browserSync.reload);
 }
 
 
+exports.htmlInclude = htmlInclude;
 exports.styles = styles;
 exports.scripts = scripts;
 exports.browsersync = browsersync;
 exports.watching = watching;
 exports.images = images;
+exports.svgSprites = svgSprites;
 exports.cleanDist = cleanDist;
 exports.build = series(cleanDist, images, build);
 
-exports.default = parallel(styles, scripts, browsersync, watching);
+exports.default = parallel(htmlInclude, svgSprites, styles, scripts, browsersync, watching);
